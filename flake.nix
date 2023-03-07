4079dec0554e5df2438b8aca16baec8a4d1a257b
Diamondy4 <Diamondy4@hotmail.com>
2023-03-07 15:29:19 +0300

    flake-parts

--- flake.nix
+++ flake.nix
@@ -3,51 +3,27 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs";
-    flake-utils.url = "github:numtide/flake-utils";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    haskell-flake.url = "github:srid/haskell-flake";
   };
 
-  outputs = { self, flake-utils, nixpkgs, ... }:
-    let
-      overlay = final: prev: {
-        haskell = prev.haskell // {
-          packageOverrides = hfinal: hprev:
-            prev.haskell.packageOverrides hfinal hprev // {
-              hedis-streams-core = hfinal.callCabal2nix "hedis-streams-core" ./lib/hedis-streams-core/. { };
-              hedis-streams-streamly = hfinal.callCabal2nix "hedis-streams-streamly" ./lib/hedis-streams-streamly/. { };
-              hedis-streams-streamly-aeson = hfinal.callCabal2nix "hedis-streams-streamly-aeson" ./lib/hedis-streams-streamly-aeson/. { };
-              hedis-streams-streamly-store = hfinal.callCabal2nix "hedis-streams-streamly-store" ./lib/hedis-streams-streamly-store/. { };
-            };
-        };
-      };
-
-      perSystem = system:
-        let
-          pkgs = import nixpkgs { inherit system; overlays = [ overlay ]; };
-          hspkgs = pkgs.haskell.packages.ghc926;
-        in
-        {
-          devShell = hspkgs.shellFor {
-            withHoogle = true;
-            packages = p: [
-              p.hedis-streams-core
-              p.hedis-streams-streamly
-              p.hedis-streams-streamly-aeson
-              p.hedis-streams-streamly-store
-            ];
-            buildInputs = [
-              hspkgs.cabal-install
-              hspkgs.haskell-language-server
-              hspkgs.fourmolu
-              pkgs.bashInteractive
-            ];
-          };
+  outputs = inputs@{ self, flake-parts, nixpkgs, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = nixpkgs.lib.systems.flakeExposed;
+      imports = [
+        inputs.haskell-flake.flakeModule
+      ];
+      perSystem = { self', config, inputs', pkgs, lib, ... }: {
+        haskellProjects.default = {
+          basePackages = pkgs.haskell.packages.ghc926;
           packages = {
-            core = hspkgs.hedis-streams-core;
-            streamly = hspkgs.hedis-streams-streamly;
-            streamly-aeson = hspkgs.hedis-streams-streamly-aeson;
-            streamly-store = hspkgs.hedis-streams-streamly-store;
+            hedis-streams-core.root = ./lib/hedis-streams-core/.;
+            hedis-streams-streamly.root = ./lib/hedis-streams-streamly/.;
+            hedis-streams-streamly-aeson.root = ./lib/hedis-streams-streamly-aeson/.;
+            hedis-streams-streamly-store.root = ./lib/hedis-streams-streamly-store/.;
           };
         };
-    in
-    { inherit overlay; } // flake-utils.lib.eachDefaultSystem perSystem;
+        packages.default = config.packages.hedis-streams-core;
+      };
+    };
 }
